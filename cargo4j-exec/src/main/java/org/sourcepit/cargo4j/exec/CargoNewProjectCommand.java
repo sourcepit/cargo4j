0dcedb79fb586b7c6682698a7012328d0002167c
Bernd Vogt <bernd.vogt@sourcepit.org>
2018-07-23 21:24:01 +0200

    Add CargoNewProjectCommand

---
+++ cargo4j-exec/src/main/java/org/sourcepit/cargo4j/exec/CargoNewProjectCommand.java
@@ -0,0 +1,31 @@
+package org.sourcepit.cargo4j.exec;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.exec.CommandLine;
+import org.apache.commons.exec.DefaultExecutor;
+import org.apache.commons.exec.PumpStreamHandler;
+
+public class CargoNewProjectCommand {
+
+	public void execute(File cargoProject) throws IOException {
+
+		final CommandLine cmdLine = CommandLine.parse("cargo new --bin \"" + cargoProject.getName() + "\"");
+
+		final ByteArrayOutputStream err = new ByteArrayOutputStream();
+		final ByteArrayOutputStream out = new ByteArrayOutputStream();
+		final PumpStreamHandler streamHandler = new PumpStreamHandler(out, err);
+
+		final DefaultExecutor executor = new DefaultExecutor();
+		executor.setWorkingDirectory(cargoProject.getParentFile());
+		executor.setStreamHandler(streamHandler);
+
+		final int exitValue = executor.execute(cmdLine);
+		if (exitValue != 0) {
+			final String errorMessage = new String(err.toByteArray(), "UTF-8");
+			throw new IOException(errorMessage);
+		}
+	}
+}
